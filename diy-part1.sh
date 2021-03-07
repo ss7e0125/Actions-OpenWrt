1e0a642889d9d55a27cbbc8206cfb063d27c05f0
ss7e0125 <64672725+ss7e0125@users.noreply.github.com>
2021-03-07 20:16:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' ./feeds.conf.default
-sed -i '$a src-git rosy https://github.com/rosywrt/luci-theme-rosy ./feeds.conf.default
+sed -i '$a src-git rosy https://github.com/rosywrt/luci-theme-rosy' ./feeds.conf.default
